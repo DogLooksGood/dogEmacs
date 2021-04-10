ab188da139ca21c196f36c76af75103698a519fb
DogLooksGood <doglooksgood@gmail.com>
2021-04-10 22:52:41 +0800

    :fire: remove useless code

--- pdump-init.el
+++ pdump-init.el
@@ -1,9 +1,6 @@
 ;; Restore the load path
 (setq load-path +pdumped-load-path)
 
-;; Disable error message
-(setq warning-minimum-level :emergency)
-
 ;; These two modes are disabled in pdump
 (global-font-lock-mode t)
 (transient-mark-mode t)
