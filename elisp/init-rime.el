5f0f979c3076bfbf375255f83db460b0ca1d6853
DogLooksGood <doglooksgood@gmail.com>
2021-04-11 01:14:18 +0800

    :keyboard: C-SPC toggle-input-method

--- elisp/init-rime.el
+++ elisp/init-rime.el
@@ -18,6 +18,7 @@
 (autoload #'toggle-input-method "rime" nil t)
 
 (with-eval-after-load "rime"
+  (global-set-key (kbd "C-SPC") 'toggle-input-method)
   (define-key rime-active-mode-map [tab] 'rime-inline-ascii)
   (define-key rime-mode-map (kbd "C-`") 'rime-send-keybinding)
   (define-key rime-mode-map (kbd "M-j") 'rime-force-enable))
