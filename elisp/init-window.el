3db02062ce66b38097310bb43322d2e2a84b71c1
DogLooksGood <doglooksgood@gmail.com>
2021-04-11 11:16:42 +0800

    :sparkles: +split-window-dwim

--- elisp/init-window.el
+++ elisp/init-window.el
@@ -1,6 +1,16 @@
 ;;; -*- lexical-binding: t -*-
 
+(defun +split-window-dwim ()
+  "Split window.
+
+If the window is wide enough, split at right, otherwise split at below."
+  (interactive)
+  (if (> (window-width) 160)
+      (split-window-right 80)
+    (split-window-below)))
+
 (defun +rotate-window ()
+  "Rotate all windows clockwise."
   (interactive)
   (let* ((wl (window-list nil nil (minibuffer-window)))
          (bl (reverse (mapcar (lambda (w) (window-buffer w)) wl)))
