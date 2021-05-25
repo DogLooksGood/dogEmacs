19db80f7f2cfe95fb147957cf1146ca7541261ca
tianshu <doglooksgood@gmail.com>
2021-05-25 21:14:48 +0800

    update modeline

--- elisp/init-modeline.el
+++ elisp/init-modeline.el
@@ -3,10 +3,11 @@
 (setq-default mode-line-format
               '((:eval (meow-indicator))
                 (:eval (rime-lighter))
-                " %l:%C  "
+                " %l:%c "
                 (:eval (+smart-file-name-cached))
-                "  "
+                " "
                 mode-name
+                " "
                 (vc-mode vc-mode)))
 
 (provide 'init-modeline)
