85923b23e58d60dafcf968f7781a32eff8cbb87d
tianshu <doglooksgood@gmail.com>
2021-06-24 10:51:25 +0800

    bugfix

--- elisp/init-xterm.el
+++ elisp/init-xterm.el
@@ -1,23 +1,19 @@
-(require 'xterm)
-
-(setq wl-copy-process nil)
-
 (defun wl-copy (text)
-  (setq wl-copy-process (make-process :name "wl-copy"
-                                      :buffer nil
-                                      :command '("wl-copy" "-f" "-n")
-                                      :connection-type 'pipe))
-  (process-send-string wl-copy-process text)
-  (process-send-eof wl-copy-process))
+  (let ((wl-copy-process (make-process :name "wl-copy"
+                                       :buffer nil
+                                       :command '("wl-copy" "-f" "-n")
+                                       :connection-type 'pipe)))
+    (process-send-string wl-copy-process text)
+    (process-send-eof wl-copy-process)))
 
 (defun wl-copy-primary ()
   (when (use-region-p)
-    (setq wl-copy-process (make-process :name "wl-copy"
-                                        :buffer nil
-                                        :command '("wl-copy" "-p" "-f" "-n")
-                                        :connection-type 'pipe))
-    (process-send-string wl-copy-process (buffer-substring-no-properties (region-beginning) (region-end)))
-    (process-send-eof wl-copy-process)))
+    (let ((wl-copy-process (make-process :name "wl-copy"
+                                         :buffer nil
+                                         :command '("wl-copy" "-p" "-f" "-n")
+                                         :connection-type 'pipe)))
+      (process-send-string wl-copy-process (buffer-substring-no-properties (region-beginning) (region-end)))
+      (process-send-eof wl-copy-process))))
 
 (defun wl-paste ()
   (if (and wl-copy-process (process-live-p wl-copy-process))
