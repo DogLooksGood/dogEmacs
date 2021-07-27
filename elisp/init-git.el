26c1e7ab1995a71029653bb27ef3e5c564e8ce93
tianshu <doglooksgood@gmail.com>
2021-07-27 22:14:51 +0800

    update

--- elisp/init-git.el
+++ elisp/init-git.el
@@ -7,13 +7,7 @@
 ;;; magit
 
 (with-eval-after-load "magit"
-  (define-key transient-base-map (kbd "<escape>") #'transient-quit-one)
-
-  (global-unset-key (kbd "C-x g"))
-  (global-set-key (kbd "C-x g s") 'magit-status)
-  (global-set-key (kbd "C-x g b") 'magit-blame)
-  (global-set-key (kbd "C-x g l") 'magit-log)
-  (global-set-key (kbd "C-x g d") 'magit-diff))
+  (define-key transient-base-map (kbd "<escape>") #'transient-quit-one))
 
 (autoload #'magit-status "magit" nil t)
 (autoload #'magit-diff "magit" nil t)
