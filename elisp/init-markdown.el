3e92e650270bd79cdd107dee94daeac59fee9b1e
tianshu <doglooksgood@gmail.com>
2021-12-08 11:37:54 +0800

    update

--- elisp/init-markdown.el
+++ elisp/init-markdown.el
@@ -2,24 +2,16 @@
 
 (straight-use-package 'markdown-mode)
 
-(setq
- markdown-fontify-code-blocks-natively t)
-
-(defun +md-init ()
-  (variable-pitch-mode 1))
-
-(add-hook 'markdown-mode-hook #'+md-init)
-
-(setq markdown-list-item-bullets '("•" "·"))
+(setq markdown-fontify-code-blocks-natively t
+      markdown-list-item-bullets '("•" "·"))
 
 (with-eval-after-load "markdown-mode"
-  (add-hook 'org-mode-hook 'flyspell-mode)
+  (add-hook 'markdown-mode-hook 'flyspell-mode)
+  (add-hook 'markdown-mode-hook #'markdown-toggle-markup-hiding)
+  (add-hook 'markdown-mode-hook #'variable-pitch-mode)
 
   (custom-set-faces
    '(markdown-table-face ((t :inherit 'fixed-pitch)))
-   '(markdown-code-face ((t :inherit 'fixed-pitch))))
-
-  (add-hook 'markdown-mode-hook #'markdown-toggle-markup-hiding)
-  (add-hook 'markdown-mode-hook #'variable-pitch-mode))
+   '(markdown-code-face ((t :inherit 'fixed-pitch)))))
 
 (provide 'init-markdown)
--- elisp/init-modal.el
+++ elisp/init-modal.el
@@ -39,7 +39,7 @@
    '("=" . "C-c ^")
    '("p" . project-find-file)
    '("j" . project-switch-to-buffer)
-   '("t" . "C-x t")
+   '("t" . tab-bar-switch-to-tab)
    '("l" . "C-x p p")
    '("y" . "C-x g")
    '("n" . "C-x M-n")
