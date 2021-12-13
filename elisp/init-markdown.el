bc6a536666906685f8d0651609de308c63273b52
tianshu <doglooksgood@gmail.com>
2021-12-13 08:48:52 +0800

    update

--- elisp/init-markdown.el
+++ elisp/init-markdown.el
@@ -1,11 +1,12 @@
 ;; -*- lexical-binding: t; -*-
-
+(straight-use-package 'edit-indirect)
 (straight-use-package 'markdown-mode)
 
 (setq markdown-fontify-code-blocks-natively t
       markdown-list-item-bullets '("•" "·"))
 
 (with-eval-after-load "markdown-mode"
+  (require 'edit-indirect)
   (add-hook 'markdown-mode-hook 'flyspell-mode)
   (add-hook 'markdown-mode-hook #'markdown-toggle-markup-hiding)
   (add-hook 'markdown-mode-hook #'variable-pitch-mode)
